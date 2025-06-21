7bae916ad9e640a8f83845beb65d4ad4411ca270
Re1 <108322453+re1exe@users.noreply.github.com>
2025-06-21 13:22:41 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,95 @@
+const password = "Meow"
+
+let pw = false
+
+const askPassword = () => {
+  const pass = prompt("Enter Password:");
+  if (pass === password) {
+    alert("Access Granted")
+  } else {
+    window.location.href = "https://www.google.com"
+  }
+};
+
+
+if (pw == true) {
+  window.onload = askPassword;
+} else {
+  console.log('Password disabled')
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("✅ Page loaded!");
+});
+
+function ScrollTo(scrollId) {
+  console.log(`Trying to scroll to: ${scrollId}`);
+  const element = document.getElementById(scrollId);
+  if (element) {
+    element.scrollIntoView({ behavior: 'smooth' });
+    console.log('Scroll triggered!');
+  } else {
+    console.warn(`Element with id "${scrollId}" not found.`);
+  }
+}
+
+function ipcopy() {
+  document.getElementById("ip").oncopy("play.wdpserver.com")
+}
+
+
+function redirectTo(redirectLink) {
+  console.log("Trying to redirect")
+  window.open(redirectLink, "_blank")
+}
+
+const INVITE_CODE = "QuTVx7dTWS"; 
+
+fetch(`https://discord.com/api/v9/invites/${INVITE_CODE}?with_counts=true`)
+  .then(res => res.json())
+  .then(data => {
+    const total = data.approximate_member_count;
+    const online = data.approximate_presence_count;
+    document.getElementById("count").textContent = `${total} Members (${online} Online)`;
+  })
+  .catch(() => {
+    document.getElementById("count").textContent = `Unavailable`;
+  });
+
+  function copyToClipboard() {
+  const input = document.getElementById("copyText");
+  input.select();
+  input.setSelectionRange(0, 99999);
+  document.execCommand("copy");
+
+  alert("Copied to clipboard!");
+}
+
+const Year = new Date().getFullYear()
+
+document.getElementById("copyright").innerHTML = Year;
+
+
+
+fetch("projects.json")
+    .then(response => response.json())
+    .then(projects => {
+        let placeholder = document.getElementById("wikiContent");
+        let out = "";
+
+        for (let project of projects.slice(0, 6)) {
+            out += `
+                <div class="wiki">
+                    <h4>${project.name}</h4>
+                    <p>${project.description}</p>
+                    <img src="${project.image}" alt="${project.name}">
+                    <br>
+                </div>
+            `;
+        }
+
+        placeholder.innerHTML = out;
+    })
+    .catch(error => console.error("Error fetching wiki content", error));
+
+    
